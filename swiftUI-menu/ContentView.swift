8f4970a73c766a0848e3274ed588695c3e3d95f3
kalaivani <kalaivani@192.168.1.2>
2025-02-16 21:21:57 +0530

    add new files

---
+++ swiftUI-menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI-menu
+//
+//  Created by kalaivani on 16/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, VAni!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI-menu/swiftUI_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_menuApp.swift
+//  swiftUI-menu
+//
+//  Created by kalaivani on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
